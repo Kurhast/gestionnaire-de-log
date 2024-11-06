82784401c8a0b3db140a8e79e743323cb60e6599
Nicolas <109721611+Kurhast@users.noreply.github.com>
2024-11-06 21:28:37 +0100

    code commenter + update
    Commentation du code du Main.cs
    Modification du nom du Main.cs (Form1.cs -> Main.cs)

---
+++ GestionnaireLog/Main.Designer.cs
@@ -0,0 +1,202 @@
+﻿namespace GestionnaireLog
+{
+    partial class main
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(main));
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.logSelector = new System.Windows.Forms.ListView();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.logViewer = new System.Windows.Forms.DataGridView();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.logLoader = new System.Windows.Forms.Button();
+            this.logDelete = new System.Windows.Forms.Button();
+            this.allLogDelete = new System.Windows.Forms.Button();
+            this.quitApp = new System.Windows.Forms.Button();
+            this.txtSearch = new System.Windows.Forms.TextBox();
+            this.progressBar = new System.Windows.Forms.ProgressBar();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.logViewer)).BeginInit();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.logSelector);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(289, 324);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Journaux d\'évènements";
+            // 
+            // logSelector
+            // 
+            this.logSelector.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.logSelector.HideSelection = false;
+            this.logSelector.Location = new System.Drawing.Point(6, 19);
+            this.logSelector.Name = "logSelector";
+            this.logSelector.Size = new System.Drawing.Size(277, 299);
+            this.logSelector.TabIndex = 0;
+            this.logSelector.UseCompatibleStateImageBehavior = false;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.logViewer);
+            this.groupBox2.Location = new System.Drawing.Point(307, 12);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(592, 421);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Evènements";
+            // 
+            // logViewer
+            // 
+            this.logViewer.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.logViewer.Location = new System.Drawing.Point(6, 19);
+            this.logViewer.Name = "logViewer";
+            this.logViewer.Size = new System.Drawing.Size(580, 396);
+            this.logViewer.TabIndex = 0;
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Controls.Add(this.logLoader, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.logDelete, 1, 1);
+            this.tableLayoutPanel1.Controls.Add(this.allLogDelete, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.quitApp, 0, 1);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(12, 342);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.34F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(289, 117);
+            this.tableLayoutPanel1.TabIndex = 2;
+            // 
+            // logLoader
+            // 
+            this.logLoader.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.logLoader.Location = new System.Drawing.Point(3, 3);
+            this.logLoader.Name = "logLoader";
+            this.logLoader.Size = new System.Drawing.Size(138, 52);
+            this.logLoader.TabIndex = 0;
+            this.logLoader.Text = "Charger les logs";
+            this.logLoader.UseVisualStyleBackColor = true;
+            this.logLoader.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // logDelete
+            // 
+            this.logDelete.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.logDelete.Location = new System.Drawing.Point(147, 61);
+            this.logDelete.Name = "logDelete";
+            this.logDelete.Size = new System.Drawing.Size(139, 53);
+            this.logDelete.TabIndex = 1;
+            this.logDelete.Text = "Supprimer une log";
+            this.logDelete.UseVisualStyleBackColor = true;
+            this.logDelete.Click += new System.EventHandler(this.logDelete_Click);
+            // 
+            // allLogDelete
+            // 
+            this.allLogDelete.Location = new System.Drawing.Point(147, 3);
+            this.allLogDelete.Name = "allLogDelete";
+            this.allLogDelete.Size = new System.Drawing.Size(139, 52);
+            this.allLogDelete.TabIndex = 3;
+            this.allLogDelete.Text = "Supprimer toutes les logs";
+            this.allLogDelete.UseVisualStyleBackColor = true;
+            this.allLogDelete.Click += new System.EventHandler(this.allLogDelete_Click);
+            // 
+            // quitApp
+            // 
+            this.quitApp.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.quitApp.Location = new System.Drawing.Point(3, 61);
+            this.quitApp.Name = "quitApp";
+            this.quitApp.Size = new System.Drawing.Size(138, 53);
+            this.quitApp.TabIndex = 2;
+            this.quitApp.Text = "Quitter l\'application";
+            this.quitApp.UseVisualStyleBackColor = true;
+            this.quitApp.Click += new System.EventHandler(this.quitApp_Click);
+            // 
+            // txtSearch
+            // 
+            this.txtSearch.Location = new System.Drawing.Point(313, 439);
+            this.txtSearch.Name = "txtSearch";
+            this.txtSearch.Size = new System.Drawing.Size(168, 20);
+            this.txtSearch.TabIndex = 3;
+            this.txtSearch.TextChanged += new System.EventHandler(this.txtSearch_TextChanged);
+            // 
+            // progressBar
+            // 
+            this.progressBar.Location = new System.Drawing.Point(488, 440);
+            this.progressBar.Name = "progressBar";
+            this.progressBar.Size = new System.Drawing.Size(411, 19);
+            this.progressBar.Style = System.Windows.Forms.ProgressBarStyle.Continuous;
+            this.progressBar.TabIndex = 4;
+            // 
+            // main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(911, 474);
+            this.Controls.Add(this.progressBar);
+            this.Controls.Add(this.txtSearch);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "main";
+            this.Text = "Gestionnaire de logs";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.logViewer)).EndInit();
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Button logLoader;
+        private System.Windows.Forms.Button logDelete;
+        private System.Windows.Forms.ListView logSelector;
+        private System.Windows.Forms.DataGridView logViewer;
+        private System.Windows.Forms.TextBox txtSearch;
+        private System.Windows.Forms.Button allLogDelete;
+        private System.Windows.Forms.Button quitApp;
+        private System.Windows.Forms.ProgressBar progressBar;
+    }
+}
+
---
+++ GestionnaireLog/Main.cs
@@ -0,0 +1,292 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.Security.Principal;
+
+namespace GestionnaireLog
+{
+    public partial class main : Form
+    {
+        public main()
+        {
+            InitializeComponent();          // Initialise tout les composants de l'interface
+            InitLogSelec();                 // Initialise les catégories de logs
+            LoadLogSelec();                 // Charge les caégories de logs
+
+            progressBar.Visible = false;    // Cache la barre de progression par défaut
+
+            // Vérifie si l'application est en mode administrateur
+            if (IsRunningAsAdministrator())
+            {
+                // Affiche un message quand l'application est lancer en mode administrateur
+                MessageBox.Show("L'application est maintenant en mode administrateur.", "Mode Administrateur", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+
+        // Configure les catégories de logs pour les affichers sous forme de liste
+        private void InitLogSelec()
+        {
+            logSelector.View = View.Details;                       // Vue détaillée des catégories
+            logSelector.FullRowSelect = true;                      // Selection de toute la ligne
+            logSelector.HeaderStyle = ColumnHeaderStyle.None;      // Pas de titre de colonne
+            logSelector.Columns.Add("", -2);                       // Colonne vide donc invisible sur l'UI
+        }
+
+        // Charge les logs en fonction de la catégorie 
+        private void LoadLogSelec()
+        {
+            EventLog[] evenLogs = EventLog.GetEventLogs();         // Récupère toutes les logs
+
+            foreach (EventLog log in evenLogs)                     // Parcours les logs une par une
+            {
+                ListViewItem item = new ListViewItem(log.Log);     // Crée un élément pour chaque logs
+                logSelector.Items.Add(item);                       // Ajoute l'élément crée à la catégorie choisis
+            }
+        }
+
+        // Bouton qui déclanche le chargement des logs
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // Vérifie si une log est sélectionné
+            if (logSelector.SelectedItems.Count > 0)
+            {
+                // Récupère la catégorie sélectionné
+                string selectedLog = logSelector.SelectedItems[0].Text;
+
+                // Si l'application a besoin des droits administrateur ET que l'application n'est pas en mode administrateur
+                if (RequiresAdmin(selectedLog) && !IsRunningAsAdministrator())
+                {
+                    // Affiche un message qui demande si on veut reboot l'application en mode administrateur automatiquement
+                    DialogResult result = MessageBox.Show(
+                        "Cette action nécessite des droits administrateur. L'application va redémarrer en mode administrateur. Voulez-vous continuer ?",
+                        "Confirmation",
+                        MessageBoxButtons.YesNo,
+                        MessageBoxIcon.Information
+                    );
+
+                    if (result == DialogResult.Yes) // Si l'utilisateur accepte de reboot
+                    {
+                        RestartAsAdministrator();   // Reboot l'application
+                    }
+                    return;                         
+                }
+
+                LoadLog(selectedLog);               // Charge les logs de la catégorie sélectionné
+            }
+            else
+            {  
+                // Affiche un message qui demande de choisir une log si le boutton charger a été appuyer sans selection d'une catégorie
+                MessageBox.Show("Veuillez sélectionner une log à charger.");
+            }
+        }
+
+        // Vérification que la catégorie "Security" demande les droits administrateur 
+        private bool RequiresAdmin(string logName)
+        {
+            return logName.Equals("Security", StringComparison.OrdinalIgnoreCase);
+        }
+
+        // Vérifie si l'application est en mode administrateur
+        private bool IsRunningAsAdministrator()
+        {
+            WindowsIdentity identity = WindowsIdentity.GetCurrent();        // Vérifie l'état actuel
+            WindowsPrincipal principal = new WindowsPrincipal(identity);
+            return principal.IsInRole(WindowsBuiltInRole.Administrator);    // Vérifie le rôle administrateur
+        }
+
+        // Reboot l'application avec les droits administrateur
+        private void RestartAsAdministrator()
+        {
+            ProcessStartInfo startInfo = new ProcessStartInfo
+            {
+                FileName = Application.ExecutablePath,     // Chemin du .exe de l'application
+                UseShellExecute = true,
+                Verb = "runas" 
+            };
+
+            try
+            {
+                Process.Start(startInfo);                  // Lance l'application en mode admin
+                Application.Exit();                        // Quitte l'application qui est en mode user   
+            }
+            catch (Exception)
+            { 
+                // Si le reboot ne marche pas, affiche un message d'erreur
+                MessageBox.Show("L'élévation des droits administrateur a été annulée ou a échoué.", "Erreur", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        // Charge et affiche les logs d'une catégorie
+        private async void LoadLog(string logName)
+        {
+            try
+            {       
+                InitLogViewer();                            // Initialise l'affichage des logs
+                logViewer.Rows.Clear();                     // Clear toutes les lignes de logs
+                logViewer.Columns.Clear();                  // Clear toutes les colonnes de logs
+
+                EventLog eventLog = new EventLog(logName);  // Récupère la log
+
+                int entryCount = eventLog.Entries.Count;    // Compte le nombre de logs récupéré
+                if (entryCount == 0)
+                {   
+                    // Si aucune logs est trouvé, affiche un message
+                    MessageBox.Show($"Aucune entrée trouvée dans le journal '{logName}'.", "Avertissement", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    return;
+                }
+
+                // Ajoute les colonnes pour l'affichage des logs
+                logViewer.Columns.Add("TimeGenerated", "Date et Heure");
+                logViewer.Columns.Add("EntryType", "Type");
+                logViewer.Columns.Add("Source", "Source");
+                logViewer.Columns.Add("Message", "Message");
+
+                progressBar.Visible = true;                 // Affiche la barre de progression
+                progressBar.Value = 0;                      // Initialisation de la barre de progression à 0
+
+                // Traitement asynchrone de l'affichage des logs
+                await Task.Run(async () =>
+                {
+                    int progress = 0;
+                    foreach (EventLogEntry entry in eventLog.Entries)
+                    {
+                        Invoke(new Action(() =>
+                            logViewer.Rows.Add(entry.TimeGenerated, entry.EntryType, entry.Source, entry.Message)
+                        )); // Ajoute les infos de la log
+
+                        progress++; 
+                        int percent = (progress * 100) / entryCount;            // Calcule la progression de la barre
+                        Invoke(new Action(() => progressBar.Value = percent));
+                    }
+                });
+
+                progressBar.Value = 100; // Force la barre à monter à 100% pour l'UI
+            }
+            catch (Exception ex)
+            {  
+                // Affiche une erreur sur le chargement des logs échoue
+                MessageBox.Show("Erreur :" + ex.Message);
+            }
+            finally
+            {
+                await Task.Delay(200);          // Ajout d'un délais sur le chargement pour la fluidité de la barre de progression
+                progressBar.Visible = false;    // Cache la barre de progression
+            }
+        }
+
+        // Configuration de l'affichage des logs
+        private void InitLogViewer()
+        {
+            logViewer.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;     // Remplis automatiquement les colonnes
+            logViewer.AllowUserToAddRows = false;                                     // Désactive l'ajout de lignes manuelle
+            logViewer.ReadOnly = true;                                                // Affiche les logs en lecture seul
+        }   
+
+        // Boutton qui ferme l'application
+        private void quitApp_Click(object sender, EventArgs e)
+        {
+            this.Close(); // Ferme l'application
+        }
+
+        // /!\ IL N'EST PAS POSSIBLE DE SUPPRIMER UNE LOG SEUL, LES FONCTIONS DE SUPPRESSION SUPPRIME DONC UNIQUEMENT /!\
+        // /!\ VISUELLEMENT LES LOGS, SI ON RELANCE L'APPLICATION, ELLES SERONT DE NOUVEAU PRESENTE ! /!\
+
+        // Button qui supprime un ou plusieur lignes de logs
+        private void logDelete_Click(object sender, EventArgs e)
+        {
+            if (logViewer.SelectedRows.Count > 0)
+            {
+                // Affiche un message pour confirmer la supression de la log
+                DialogResult result = MessageBox.Show("Êtes-vous sûr de vouloir supprimer cette ligne ?",
+                                                      "Confirmation de suppression",
+                                                      MessageBoxButtons.YesNo,
+                                                      MessageBoxIcon.Warning);
+
+                if (result == DialogResult.Yes)
+                {
+                    // Supprime là ou les lignes de logs sélectionné
+                    foreach (DataGridViewRow row in logViewer.SelectedRows)
+                    {
+                        logViewer.Rows.Remove(row);
+                    }
+                }
+            }
+            else
+            { 
+                // Affiche un message si aucune logs est selectioné
+                MessageBox.Show("Aucune ligne sélectionnée à supprimer.", "Erreur", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        // Filtre les logs en fonction de l'entrée dans la barre de recherche
+        private void txtSearch_TextChanged(object sender, EventArgs e)
+        {
+            string searchTerm = txtSearch.Text.ToLower();   // Convertit l'entrée en minuscule pour la comparaison
+
+            // Parcours chaque ligne des logs affichés
+            foreach (DataGridViewRow row in logViewer.Rows)
+            {
+                if (row.Cells["EntryType"].Value != null)
+                {
+                    bool isVisible = row.Cells["EntryType"].Value.ToString().ToLower().Contains(searchTerm);
+                    row.Visible = isVisible; // Rend la ligne visible/invisible
+                }
+                else
+                {
+                    row.Visible = false;
+                }
+            }
+        }
+
+        // Supprime toutes les lignes de la catégorie sélectionné
+        private async void allLogDelete_Click(object sender, EventArgs e)
+        {
+            // Vérifie si il y'a des lignes à supprimer
+            if (logViewer.Rows.Count > 0)
+            {
+                progressBar.Visible = true;    // Affiche la barre de progression
+                progressBar.Value = 0;         // Initialisation de la barre de progression à 0
+
+                int rowCount = logViewer.Rows.Count; // Stock le nombre de ligne à supprimer
+
+                // Lancement de la suppression
+                await Task.Run(() =>
+                {
+                    int progress = 0; // Progression de la suppression pour la barre de progression
+
+                    // Boucle tant qu'il y'a des logs à supprimer
+                    while (logViewer.Rows.Count > 0)
+                    {
+                        // Mise à jour de l'UI
+                        Invoke(new Action(() =>
+                        {
+                            logViewer.Rows.RemoveAt(0); // Supprime la première ligne de l'index
+                        }));
+
+                        progress++;
+                        int percent = (progress * 100) / rowCount;                // Calcule du pourcentage de la progression
+                        Invoke(new Action(() => progressBar.Value = percent));    // Met à jour l'état de la barre de progression
+                    }
+                });
+
+                progressBar.Value = 100;            // Force la barre à monter à 100% pour l'UI
+                progressBar.Visible = false;        // Cache la barre de progression
+
+                // Affiche un message après la suppression des logs
+                MessageBox.Show("Toutes les logs ont été supprimées.", "Suppression réussie", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else
+            {
+                // Affiche un message si il n'y a pas de logs à supprimer
+                MessageBox.Show("Aucune log à supprimer.", "Information", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+    }
+}
